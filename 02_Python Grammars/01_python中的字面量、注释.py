ab19178ea2b83fb1f29c6975aa9fa72e76bc272e
YueX1an <62510476+YueX1an@users.noreply.github.com>
2023-03-27 23:47:52 +0800

    Add files via upload

---
+++ 02_Python Grammars/01_python中的字面量、注释.py
@@ -0,0 +1,54 @@
+# 整数自变量
+"666"
+"13.14"
+"黑马程序员"
+
+print(666)
+print(13.14)
+print("黑马程序员")
+
+
+# 单行注释  与井号键空一格
+
+
+"""
+多行注释 三个引号
+"""
+
+
+# python中的变量
+# 定义一个变量
+money = 50
+# 通过print语句，输出变量记录的内容
+print("钱包还有:", money)
+# 买了一个冰激凌，花费10元
+money = money - 10
+print("买了冰激凌花费10元，还剩：", money, "元")
+
+
+# 变量
+message = "hello python world"
+print(message)
+message = "python crash course world"
+print(message)
+# 变量只能包含字母，下划线和数字，可以由下划线和字母开头，但不能以数字开头，如message_1是允许的，但1_message不被允许
+# 变量名中不能出现空格，但可以用下划线隔开字母和字母，如message_greeting
+# 不能用python关键字和函数作为变量名，如print
+# 变量名应简短和具有描述性
+# 慎用小写字母i和大写字母o，容易被误认为0和1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
