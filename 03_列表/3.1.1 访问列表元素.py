c9f251ecc446ef4dfd1d4881f3a0ae9468125905
YueX1an <62510476+YueX1an@users.noreply.github.com>
2023-05-11 23:31:34 +0100

    Add files via upload

---
+++ 03_列表/3.1.1 访问列表元素.py
@@ -0,0 +1,6 @@
+# 列表是有序的集合，要访问列表的任意元素，只要将列表中的元素位置或者索引告诉Python
+# 要访问列表元素，可指出列表的名称，在指出列表索引，并将其放在[]中
+# 例如下，从列表bicycles中提取一款自行车
+bicycles = ['trek','cannondale','redline','specialize']
+print(bicycles)
+print(bicycles[0])  # 0即是列表中的第一个元素
